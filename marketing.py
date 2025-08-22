ff3e87758b6ff1fa124e8a5515f4c9276044a83a
Michael Gebregzabihe Hagos <119433784+michaelGebregzabiher@users.noreply.github.com>
2025-08-22 15:28:45 +0000

    update

--- marketing.py
+++ marketing.py
@@ -1,2 +1,34 @@
 # Decision based on probability
-print("Send email with promo" if churn_proba >= 0.5 else "Do not send an email") #note this will be done by a webservice
+import requests
+
+customer = {
+  "gender": "female",
+  "seniorcitizen": 1,
+  "partner": "yes",
+  "dependents": "yes",
+  "phoneservice": "yes",
+  "multiplelines": "no_phone_service",
+  "internetservice": "dsl",
+  "onlinesecurity": "yes",
+  "onlinebackup": "yes",
+  "deviceprotection": "no",
+  "techsupport": "yes",
+  "streamingtv": "no",
+  "streamingmovies": "no",
+  "contract": "month-to-month",
+  "paperlessbilling": "yes",
+  "paymentmethod": "electronic_check",
+  "tenure": 1,
+  "monthlycharges": 29.85,
+  "totalcharges": 29.85
+}
+
+url = "http://localhost:9696/predict"
+response = requests.post(url, json=customer)
+churn_proba = response.json()
+print("Churn Probability:", churn_proba)
+
+if churn_proba["churn_probability"] >= 0.5:
+    print("Send email with promo")
+else:
+    print("Do not send an email") #note this will be done by a webservice
